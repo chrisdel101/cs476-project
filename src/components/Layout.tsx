a42717bc15744ea523773e59bd3aa9783996896b
Chris Del <drumgod101@gmail.com>
2023-09-17 09:53:48 -0600

    Add react-bootstrap and more router work - add footer, nav and Layout components - fighting w/ router v6; downgrade to v5 coming next

---
+++ src/components/Layout.tsx
@@ -0,0 +1,11 @@
+import Navigation from "./Navigation";
+import Footer from "./Footer";
+
+const Layout = () => {
+  return <div id="layout">
+    <Navigation />
+    <Footer />
+  </div>
+}
+
+export default Layout
