54ea7bf80c97ab5a4738c2c5b6fac4d2d1dfa557
Chris Del <drumgod101@gmail.com>
2023-09-17 14:50:20 -0600

    remove email sending func

--- src/App.tsx
+++ src/App.tsx
@@ -4,51 +4,13 @@ import styled from 'styled-components'
 import Footer from './components/Footer'
 import Navigation from './components/Navigation'
 import IndexScreen from './components/IndexScreen/IndexScreen'
-import { useState } from 'react'
-import { sendEmail } from './utilities/sendEmail'
 
 const App = () => {
-  const [feedback, setFeedback] = useState("")
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("arssonist@yahoo.com")
-  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setFeedback(event.target.value)
-  }
-  const handleSubmit = () => {
-    console.log("submitting")
-    const template_params = {
-      message: feedback,
-      email: email,
-      from_name: name,
-      reply_to: "freebee.cs476@gmail.com",
-    }
-    console.log(template_params)
-    const templateId = 'template_kw5x49q'
-    sendEmail(templateId,template_params)
-  }
+  
 
   return (
     <Router>
-      <form className="test-mailing">
-        <h1>Let's see if it works</h1>
-        <div>
-          <textarea
-            id="test-mailing"
-            name="test-mailing"
-            onChange={handleChange}            
-            placeholder="Post some lorem ipsum here"
-            required
-            value={feedback}
-            style={{ width: '100%', height: '150px' }}
-          />
-        </div>
-        <input
-          type="button"
-          value="Submit"
-          className="btn btn--submit"
-          onClick={handleSubmit}
-        />
-      </form>
+    
       <Layout>
         <Navigation />
         <Switch>
