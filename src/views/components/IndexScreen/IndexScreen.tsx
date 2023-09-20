9ebeec212257b73761f58b4b3b6ecd272e13c030
Chris Del <drumgod101@gmail.com>
2023-09-20 15:55:33 -0600

    add modal

--- src/views/components/IndexScreen/IndexScreen.tsx
+++ src/views/components/IndexScreen/IndexScreen.tsx
@@ -1,29 +1,67 @@
-import styled from "styled-components"
-import useUserAutentication from "../../../controllers/hooks/authenitication/useIsUserAuthenicated"
+import styled from 'styled-components'
+import useUserAutentication from '../../../controllers/hooks/authenitication/useIsUserAuthenicated'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import { useState } from 'react'
+
 
 const Index = () => {
-    const isLoggedIn  = useUserAutentication();
-    console.log(isLoggedIn, "isLoggedIN")
-    return <PageContainer>
-    {
-        // some space fillers - to be replaced
-        Array.from(Array(3)).map((_,i) => {
-            return(<div  key={i} style={{ border: "5px white solid" }}>
-            <img  src={"https://place-puppy.com/200x200"}/>
-            </div>)
-        })
-    }
-       
+  const [show, setShow] = useState<boolean>(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+  const isLoggedIn = useUserAutentication()
+  console.log(isLoggedIn, 'isLoggedIN')
+  return (
+    <PageContainer>
+      <SytledButton variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </SytledButton>
+      <div className="demo-container" style={{display: "flex", justifyContent: "space-evenly"}}>
+        {
+            // some space fillers - to be replaced
+            Array.from(Array(3)).map((_, i) => {
+            return (
+                <div key={i} style={{ border: '5px white solid' }}>
+                <img src={'https://place-puppy.com/200x200'} />
+                </div>
+            )
+            })
+        }
+      </div>
+      <div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add An Item </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Demo to show how modal works!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      </div>
     </PageContainer>
+  )
 }
 export default Index
 
 const PageContainer = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    background-color: blue;
-    height: 100%;
-    width: 100%;
-    
+    flex-direction: column;
+  display: flex;
+  justify-content: space-evenly;
+  background-color: blue;
+  height: 100%;
+  width: 100%;
+`
+// eslint-disable-next-line @typescript-eslint/no-explicit-any 
+// disable required for this styled component only
+const SytledButton = styled(Button)<any>`
+    width: 200px;
+    align-self: center;
 `
