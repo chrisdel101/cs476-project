d66d2cb780e11ccb03bad4555d1681b2494066b3
Chris Del <drumgod101@gmail.com>
2023-11-05 12:46:53 -0600

    Add extra styles to index cards

--- src/controllers/IndexScreen/requestItemController.ts
+++ src/controllers/IndexScreen/requestItemController.ts
@@ -4,13 +4,13 @@ import crudFunctions from '../../api/crudFunctions'
 import User from '../../models/abstractClasses/User'
 import Item from '../../models/Item'
 
-export const handleRequestItem = (currentUser: User, item: Item) => {
-    if(currentUser.userType === UserTypes.DONOR) return
+export const handleRequestItem = (currentUser: User | null, item: Item) => {
+    if(currentUser?.userType === UserTypes.DONOR) return
     // confirm item is available
     if(item.itemState === ItemStates.AVAILABLE) {
         // change item state
        item.setItemState = ItemStates.PENDING
-       item.setReceiverId = currentUser.id as string
+       item.setReceiverId = currentUser?.id as string
     // update item in db
     crudFunctions.updateEntireItem(item)
     } else {
--- src/views/components/IndexScreen/ItemCard.tsx
+++ src/views/components/IndexScreen/ItemCard.tsx
@@ -4,6 +4,7 @@ import Card from 'react-bootstrap/Card'
 import Item from '../../../models/Item'
 import { handleRequestItem } from '../../../controllers/IndexScreen/requestItemController'
 import useUserContext from '../../../controllers/context/userContext/useUserContext'
+import { UserTypes } from '../../../../constants'
 
 interface IProps {
   item: Item
@@ -20,13 +21,11 @@ const ItemCard = ({ item }: IProps) => {
   const minute = addedAtTimeStamp.getMinutes()
 
   // template for the card contents
-  if (currentUser) {
+  // if (currentUser) {
     return (
       // following the information at https://getbootstrap.com/docs/4.3/components/card/ but with the boostrap 5 card elements found at //https://react-bootstrap.netlify.app/docs/components/cards
-      <Card
-        border="light"
-        style={{ width: '90%', maxHeight: '100%', margin: '10px 0' }}
-      >
+      <StyledCard
+        border="light">
         <Card.Body>
           <div className="row no-gutters">
             <ImgContainer className="col-md-3">
@@ -35,36 +34,50 @@ const ItemCard = ({ item }: IProps) => {
             </ImgContainer>
             <TextContainer className="col-md-6">
               <div className="card-body">
-                <Card.Title>{item?.name}</Card.Title>
-                <p className="card-text">{item?.description}</p>
-                <p className="card-text">{item?.itemState}</p>
+                <Card.Title>Item Name: {item?.name}</Card.Title>
+                <p className="card-text">Item description: {item?.description}</p>
+                <p className="card-text">ITEM STATUS: {item?.itemState}</p>
                 <p className="card-text">{item?.receiverId}</p>
                 <p className="card-text">
-                  <small className="text-muted">Date posted: </small>
+                  <small className="text-muted">Date added: Date posted: </small>
                   <small className="text-muted">{`${day}-${month}-${year} ${hour}:${minute}`}</small>
                 </p>
               </div>
             </TextContainer>
             {/* if item is available (checked before this function call), and the user is a receiver, user can request item */}
-            {currentUser.userType === 'receiver' ? (
-              <ButtonContainer className="col-md-3">
+            {currentUser?.userType === UserTypes.RECEIVER ? (
+              <StyledContainer className="col-md-3">
                 <StyledButton
                   variant="primary"
                   onClick={() => handleRequestItem(currentUser, item)}
                 >
                   Request Item
                 </StyledButton>
-              </ButtonContainer>
-            ) : null}
+              </StyledContainer>
+            ) : !currentUser ? 
+            
+            <StyledContainer $bgColor={true}>
+              <div>
+                Create an account to get started
+              </div>
+               
+                </StyledContainer>
+          : null
+            }
           </div>
         </Card.Body>
-      </Card>
+      </StyledCard>
     )
-  }
+  // }
 }
 
 export default ItemCard
 
+const StyledCard = styled(Card)`
+  width: 90%;
+  max-height: 100% ;
+  margin: 10px 0;
+`
 // organizes the text content of the item card
 const TextContainer = styled.div`
   flex-direction: column;
@@ -85,7 +98,7 @@ const StyledImg = styled.img`
   width: initial;
 `
 // organizes the button alignment on the item card
-const ButtonContainer = styled.div`
+const StyledContainer = styled.div<{ $bgColor?: boolean }>`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
@@ -94,6 +107,8 @@ const ButtonContainer = styled.div`
   align-content: center;
   gap: 10px;
   flex: 1;
+  background-color: ${props => props.$bgColor ? "whitesmoke" : "initial"};
+  border-radius: 5px;
 `
 // standardizes the buttons on the card
 const StyledButton = styled(Button)<any>`
